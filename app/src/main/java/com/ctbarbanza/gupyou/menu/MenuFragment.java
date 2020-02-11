b101a34a08d5af8f639907655cf2628d3a0e7964
ImartDeveloper <jorge.parad@ctbarbanza.com>
2020-02-11 19:00:20 +0100

    Añadidos Iconos - QUITADO el SPork; no funciona correctamente con la versión de Android

--- app/src/main/java/com/ctbarbanza/gupyou/menu/MenuFragment.java
+++ app/src/main/java/com/ctbarbanza/gupyou/menu/MenuFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.ctbarbanza.gupyou.R;
 
@@ -25,7 +28,18 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.frg_menu, container, false);
+        initButtons(view);
         return view;
+    }
+
+    private void initButtons(View view) {
+        ImageButton btnProfile = view.findViewById(R.id.menu_profile_btn);
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Y", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
